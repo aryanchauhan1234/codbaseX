a6153fea9324aa2737fec2c2bf54948c8fa1aecc
Aryan Chauhan <165141982+aryanchauhan1234@users.noreply.github.com>
2025-07-29 01:40:38 +0530

    Create WorkingPage.jsx

---
+++ frontend/src/pages/WorkingPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const WorkingPage = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-white px-4">
+      <div className="text-center max-w-md">
+
+        <p className="text-lg text-gray-500">
+          This page is currently under development.<br />
+          Please check back soon.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default WorkingPage;
